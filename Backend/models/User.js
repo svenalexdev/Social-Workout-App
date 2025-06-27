85e6806742adf4e07f1a2f50a53176bd748cb7e7
Ankita Malani <ankitamalani2021@gmail.com>
2025-06-27 12:41:00 +0200

    add model

---
+++ Backend/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: [true, 'Firstname is required'] },
+    email: { type: String, required: [true, 'Email is required'], unique: true },
+    password: { type: String, required: [true, 'Password is required'], select: false },
+    stats: [
+      {
+        height: { type: Number, required: false },
+        weight: { type: Number, required: false },
+        age: { type: Number, required: false }
+      }
+    ],
+    lastLoginDate: { type: Date, default: null }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model('User', userSchema);
