294d323e94f8cc2ec434f7c47c5b110c018bcb00
Ankita Malani <ankitamalani2021@gmail.com>
2025-07-01 22:22:04 +0200

    add backend login data

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import exerciseRouter from './routers/exerciseRouter.js';
 import userRouter from './routers/userRouter.js';
 import planRouter from './routers/planRouter.js';
 import logRouter from './routers/logRouter.js';
+import authRouter from './routers/authRouter.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -14,6 +15,7 @@ app.use('/exercises', exerciseRouter);
 app.use('/users', userRouter);
 app.use('/plans', planRouter);
 app.use('/logs', logRouter);
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
