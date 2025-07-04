69c20478a5d627b87d103eefcf4ccc9609c0bb15
Sven Kaack <sven.kaack@gmx.de>
2025-07-04 09:40:33 +0200

    fixed issue with exercise list and coloring marked exercises

--- Frontend/src/pages/CreatePlan.jsx
+++ Frontend/src/pages/CreatePlan.jsx
@@ -260,7 +260,7 @@ console.log(filteredExercises);
       {/* Modal / popup to show exercise list  */}
       {showExercises && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-6">
-          <div className="bg-gray-800 rounded-2xl max-w-lg w-full max-h-[90vh] overflow-y-auto overflow-hidden relative">
+          <div className="bg-gray-800 rounded-2xl max-w-lg w-full max-h-[90vh] min-h-full overflow-y-auto overflow-hidden relative">
             {/* Close Button */}
             <button
               onClick={() => {
