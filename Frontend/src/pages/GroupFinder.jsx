48d672a75d21415d3eaf6c36be52d8453821d89c
Sven Kaack <sven.kaack@gmx.de>
2025-07-07 19:00:12 +0200

    structured mapped activity card

--- Frontend/src/pages/GroupFinder.jsx
+++ Frontend/src/pages/GroupFinder.jsx
@@ -1,19 +1,23 @@
 import { useNavigate } from 'react-router';
-import { useState } from 'react';
-
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const GroupFinder = () => {
   const navigate = useNavigate();
+
+  // Mock data to be replaced with fetch later
+  // 1. Add useState for fetched data: const [activities, setActivities] = useState([])
+  // 2. Add useEffect to fetch data
+  // 3. Replace mockData[0]
 
   let mockData = [
     {
       _id: '',
       userId: 'userId of person posting',
       name: 'Chest Workout',
-      description: '',
-      gym: 'gymname',
-      time: 'Time',
+      description:
+        'This is a description of a workout activity and only mockdata. This is a description of a workout activity and only mockdata. This is a description of a workout activity and only mockdata.',
+      gym: 'McFit',
+      time: '18:00',
       showworkoutplan: 'true',
       workoutplanid: 'ID',
       'attendees-limit': 3,
@@ -102,9 +106,43 @@ const GroupFinder = () => {
       <div className="mt-10 flex">
         <h2 className="font-bold text-xl">Matching Activities</h2>
       </div>
-      <div className="mt-4 border border-gray-500 rounded-2xl h-50 w-90 mx-auto flex flex-col justify-between">
-        <img src="https://cdn-icons-png.freepik.com/512/6833/6833605.png" className="h-18 w-18 m-4 rounded-full" />
-        <div className="flex justify-center mb-3">
+      <div className="mt-4 p-3 border border-gray-500 rounded-2xl max-w-md mx-auto flex flex-col overflow-hidden">
+        <div className="flex items-center">
+          <img src="https://cdn-icons-png.freepik.com/512/6833/6833605.png" className="h-20 w-20 rounded-full" />
+          <div className="flex flex-col ml-2">
+            <p>
+              <span className="font-bold">Name: </span>
+              {mockData[0].name}
+            </p>
+            <p>
+              <span className="font-bold">Gym: </span>
+              {mockData[0].gym}
+            </p>
+            <p>
+              <span className="font-bold">Time: </span>
+              {mockData[0].time}
+            </p>
+          </div>
+        </div>
+        <div className="attendees flex mt-2">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/024/183/525/non_2x/avatar-of-a-man-portrait-of-a-young-guy-illustration-of-male-character-in-modern-color-style-vector.jpg"
+            className="ml-22 h-8 w-8 rounded-full"
+          />
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/004/899/680/non_2x/beautiful-blonde-woman-with-makeup-avatar-for-a-beauty-salon-illustration-in-the-cartoon-style-vector.jpg"
+            className="ml-2 h-8 w-8 rounded-full"
+          />
+          <img
+            src="https://img.freepik.com/vektoren-kostenlos/die-veranschaulichung-des-laechelnden-jungen_1308-174669.jpg?semt=ais_hybrid&w=740"
+            className="ml-2 h-8 w-8 rounded-full"
+          />
+        </div>
+        {/* Fetch description */}
+        <p className="mt-4 max-h-16 overflow-hidden break-words">
+          {mockData[0].description.length > 90 ? mockData[0].description.slice(0, 90) + '...' : mockData[0].description}
+        </p>
+        <div className="flex justify-center mt-5">
           <button className="btn bg-gray-500 h-8">See Workout Plan</button>
           <button className="btn bg-gray-500 ml-5 h-8">Ask to Join</button>
         </div>
